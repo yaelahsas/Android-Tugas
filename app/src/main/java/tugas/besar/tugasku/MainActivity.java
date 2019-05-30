37737f73d1395a990ca44a5f75b676b76168f08a
novianurulumami <nummynovia12@gmail.com>
2019-05-30 09:49:05 +0700

    mengganti/revisi desain layout daftar, login, dan main. dan alur manifests

--- app/src/main/java/tugas/besar/tugasku/MainActivity.java
+++ app/src/main/java/tugas/besar/tugasku/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         Thread thread = new Thread(){
             public void run(){
                 try{
-                    sleep(2000);
+                    sleep(1000);
                 } catch (InterruptedException e){
                     e.printStackTrace();
                 }finally {
