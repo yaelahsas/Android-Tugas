68441ef0fd7466df9eee829a3c19ec3ffc5bbb44
yaelahsas <tumbalig001@gmail.com>
2019-06-01 15:20:50 +0700

    1. Penambahan Gradle 2. Penambahan Logout 3. Penambahan Current Location 4. Perbaikan Tampilan Login 5. Menyimpan data nama, telpon dan jenis kelamin ketika daftar 6. Perbaikan lain-lain

--- app/src/main/java/tugas/besar/tugasku/Daftar.java
+++ app/src/main/java/tugas/besar/tugasku/Daftar.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -19,30 +20,41 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Daftar extends AppCompatActivity implements View.OnClickListener {
 
-    private  EditText editEmail, editPass;
+    private DatabaseReference mNoteRef;
+    private  EditText editEmail, editPass,namanya,telpon,editPassRe;
     private Button tblDaftar;
     private TextView textDaftar;
     private ProgressDialog progressDialog;
     private FirebaseAuth mAuth;
+    private CheckBox lk,pr;
+    private String nama, jk,tlp,emailnya;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
 
+        mNoteRef = FirebaseDatabase.getInstance().getReference("Pengguna");
         mAuth = FirebaseAuth.getInstance();
 
         progressDialog = new ProgressDialog(this);
 
 
-        editEmail = (EditText)findViewById(R.id.daftarEmail);
-        editPass = (EditText)findViewById(R.id.daftarPass);
-        tblDaftar = (Button)findViewById(R.id.tombolDaftar);
-        textDaftar = (TextView)findViewById(R.id.tulisanPunyaAkun);
 
+        namanya = findViewById(R.id.daftarNama);
+        telpon = findViewById(R.id.daftarHp);
+        editEmail = findViewById(R.id.daftarEmail);
+        editPass = findViewById(R.id.daftarPass);
+        editPassRe = findViewById(R.id.daftarPassRe);
+        tblDaftar = findViewById(R.id.tombolDaftar);
+        textDaftar = findViewById(R.id.tulisanPunyaAkun);
+        lk = findViewById(R.id.daftarLaki);
+        pr = findViewById(R.id.daftarPerempuan);
         tblDaftar.setOnClickListener(this);
         textDaftar.setOnClickListener(this);
 
@@ -51,8 +63,46 @@ public class Daftar extends AppCompatActivity implements View.OnClickListener {
 
     private void daftarUser(){
 
-        String emailnya = editEmail.getText().toString().trim();
+       final String key = mNoteRef.push().getKey();
+        nama = namanya.getText().toString().trim();
+        tlp = telpon.getText().toString().trim();
+        emailnya = editEmail.getText().toString().trim();
         String passnya = editPass.getText().toString().trim();
+        String repassnya = editPassRe.getText().toString().trim();
+
+        if (TextUtils.isEmpty(nama)){
+
+            Toast.makeText(this,"Masukkan Nama ",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (lk.isChecked()){
+
+            jk = "Laki-Laki";
+
+        }
+        if (pr.isChecked()){
+
+            jk = "Perempuan";
+
+        }
+        if(!lk.isChecked() && !pr.isChecked()){
+
+            Toast.makeText(this,"Masukkan Jenis Kelamin",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(lk.isChecked() && pr.isChecked()){
+
+            Toast.makeText(this,"Pilih Satu Jenis Kelamin",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(tlp)){
+
+            Toast.makeText(this,"Masukkan Telpon ",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
 
         if (TextUtils.isEmpty(emailnya)){
 
@@ -67,10 +117,17 @@ public class Daftar extends AppCompatActivity implements View.OnClickListener {
               return;
         }
 
+
+        if (!repassnya.equals(passnya)){
+            Toast.makeText(this,"Password Tidak Sama",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         //jika lolos validasi
 
         progressDialog.setMessage("Sedang Mendaftar...");
         progressDialog.show();
+
 
         mAuth.createUserWithEmailAndPassword(emailnya, passnya)
             .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -80,10 +137,18 @@ public class Daftar extends AppCompatActivity implements View.OnClickListener {
 
                         //User berhasil dibuat
                         progressDialog.hide();
+                        Pengguna pengguna = new Pengguna(key,nama,jk,tlp);
+                        mNoteRef.child(key).setValue(pengguna);
                         Toast.makeText(Daftar.this,"Daftar Berhasil",Toast.LENGTH_SHORT).show();
                         editEmail.setText("");
                         editPass.setText("");
-
+                        namanya.setText("");
+                        telpon.setText("");
+                        editPassRe.setText("");
+                        lk.setChecked(false);
+                        pr.setChecked(false);
+                        Intent login = new Intent(Daftar.this, Login.class);
+                        startActivity(login);
                     } else {
                         progressDialog.hide();
                         Toast.makeText(Daftar.this,"Daftar Gagal.. Coba Lagi",Toast.LENGTH_SHORT).show();
